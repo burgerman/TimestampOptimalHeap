cd711efc7d608248ac88bb5a2d2650904eab3864
Menghao <737330422@qq.com>
2025-07-19 13:11:15 -0400

    moved heap and dijkstra to another file

---
+++ src/WorkSetHeap.h
@@ -0,0 +1,9 @@
+//
+// Created by Wilfried Wu on 2025-07-19.
+//
+
+#ifndef TIMESTAMPOPTIMALHEAP_WORKSETHEAP_H
+#define TIMESTAMPOPTIMALHEAP_WORKSETHEAP_H
+void func_perform_test();
+void example_dijkstra();
+#endif //TIMESTAMPOPTIMALHEAP_WORKSETHEAP_H
